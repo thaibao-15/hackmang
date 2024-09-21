4500b06eaf95369e58c208314d13b05c68191791
thaibao-15 <phamthaibao2005thd@gmail.com>
2024-09-21 16:33:33 +0700

    Add files via upload

---
+++ 224THJAVA/TUAN2_BAO/b1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println( " mời nhập tên");
+        String ten = sc.nextLine();
+        System.out.println(" hello " + tên );
+    }
+}
